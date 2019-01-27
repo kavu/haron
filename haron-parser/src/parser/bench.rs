584e4d62d6b9615df6ad405293e7c07a255e4253
Max Riveiro <kavu13@gmail.com>
2019-01-27 16:27:39 +0300

    Add test::black_box to parse_string bench

--- haron-parser/src/parser/bench.rs
+++ haron-parser/src/parser/bench.rs
@@ -6,5 +6,7 @@ use super::parse_string;
 fn name(b: &mut test::Bencher) {
     let input = "\r\n\r\nDAT 1, 1  \n\nMOV 1\n\n\nSPL ,1\n".into();
 
-    b.iter(|| parse_string(input))
+    b.iter(|| {
+        test::black_box(parse_string(input))
+    })
 }
