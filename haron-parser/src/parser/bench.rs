23ce59ca07b768eaacc0fc719284beb3a010f3d8
Max Riveiro <kavu13@gmail.com>
2019-01-28 20:21:13 +0300

    Rename parse_string bench

--- haron-parser/src/parser/bench.rs
+++ haron-parser/src/parser/bench.rs
@@ -3,7 +3,7 @@ extern crate test;
 use super::parse_string;
 
 #[bench]
-fn name(b: &mut test::Bencher) {
+fn bench_parse_string(b: &mut test::Bencher) {
     let input = "\r\n\r\nDAT 1, 1  \n\nMOV 1\n\n\nSPL ,1\n".into();
 
     b.iter(|| {
